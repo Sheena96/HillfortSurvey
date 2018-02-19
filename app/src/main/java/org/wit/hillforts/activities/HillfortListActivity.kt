d967ee0f674738425f8054dc5ca2b53147edf406
Sheena Kelly <sheena.96@hotmail.com>
2018-02-19 20:07:58 +0000

    Adding the card view and recycler view

--- app/src/main/java/org/wit/hillforts/activities/HillfortListActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfortListActivity.kt
@@ -2,7 +2,17 @@ package org.wit.hillforts.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_hillfort.*
+import kotlinx.android.synthetic.main.activity_hillfort.view.*
+import kotlinx.android.synthetic.main.activity_hillfort_list.*
+import kotlinx.android.synthetic.main.card_hillfort.view.*
 import org.wit.hillforts.main.MainApp
+import org.wit.hillforts.models.HillfortModel
 import org.wit.hillfortsurvey.R
 
 class HillfortListActivity : AppCompatActivity() {
@@ -14,5 +24,32 @@ class HillfortListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_hillfort_list)
         app = application as MainApp
 
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = HillfortAdapter(app.hillforts)
+    }
+
+    class HillfortAdapter constructor(private var hillforts: List<HillfortModel>) : RecyclerView.Adapter<HillfortAdapter.MainHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MainHolder {
+            return MainHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_hillfort, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: MainHolder, position: Int) {
+            val hillfort = hillforts[holder.adapterPosition]
+            holder.bind(hillfort)
+        }
+
+        override fun getItemCount(): Int = hillforts.size
+
+        class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            fun bind(hillfort: HillfortModel) {
+                itemView.hillfortTownland.text = hillfort.townland
+                itemView.hillfortCounty.text = hillfort.county
+            }
+        }
     }
 }
+
+
