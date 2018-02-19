5b4b71a6ab4b3f5c7a34ad52f63059ec34e3b154
Sheena Kelly <sheena.96@hotmail.com>
2018-02-19 17:57:00 +0000

    Added MainApp class

--- app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
@@ -6,24 +6,42 @@ import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
+import org.wit.hillforts.main.MainApp
 import org.wit.hillforts.models.HillfortModel
 import org.wit.hillfortsurvey.R
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     var hillfort = HillfortModel()
+    lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+        app = application as MainApp
 
         btnAdd.setOnClickListener() {
-           hillfort.title = hillfortTitle.text.toString()
-            if (hillfort.title.isNotEmpty()) {
-                info("add Button Pressed: $hillfortTitle")
+           hillfort.townland = hillfortTownland.text.toString()
+            hillfort.county = hillfortCounty.text.toString()
+
+            if (hillfort.townland.isNotEmpty()) {
+                //info("add Button Pressed: $hillfortTownland")
+                app.hillforts.add(hillfort.copy())
+                info("add Button Pressed: $hillfortTownland")
+                app.hillforts.forEach { info("add Button Pressed: ${it}")}
             }
             else {
-                toast ("Please Enter a Hillfort")
+                toast ("Please Enter a Townland")
+            }
+
+            if (hillfort.county.isNotEmpty()) {
+                //info("add Button Pressed: $hillfortCounty")
+                app.hillforts.add(hillfort.copy())
+                info("add Button Pressed: $hillfortCounty")
+                app.hillforts.forEach { info("add Button Pressed: ${it}")}
+            }
+            else {
+                toast ("Please Enter a County")
             }
         }
 
--- app/src/main/java/org/wit/hillforts/models/HillfortModel.kt
+++ app/src/main/java/org/wit/hillforts/models/HillfortModel.kt
@@ -1,4 +1,5 @@
 package org.wit.hillforts.models
 
 
-data class HillfortModel(var title: String = "")
+data class HillfortModel(var townland: String = "",
+                         var county: String = "")
